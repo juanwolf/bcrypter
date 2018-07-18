2294705531726c18e08d413c7453d1d184eb94f7
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2018-07-18 23:41:59 +0100

    :sparkles:(bcrypter): Add second arg for cost

--- bcrypter.go
+++ bcrypter.go
@@ -2,11 +2,17 @@ package main
 
 import "os"
 import "fmt"
+import "strconv"
 import "golang.org/x/crypto/bcrypt"
 
 func main() {
 	password := os.Args[1]
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	cost := bcrypt.DefaultCost
+	var err error
+	if len(os.Args) > 2 {
+		cost, err = strconv.Atoi(os.Args[2])
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
 	if err != nil {
 		panic(err)
 	}
